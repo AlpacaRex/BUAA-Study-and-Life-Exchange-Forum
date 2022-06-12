92940cc5243bba97f1a9c080a8ec0ce050d84bfa
AlpacaRex <1030519668@qq.com>
2022-06-12 22:49:44 +0800

    edit time

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from user.models import User
 
-# Create your tests here.
+
+class loginTest(TestCase):
+
+    def setUp(self):
+        User.objects.create(id=20373615, password=20373615, security_issue="20373615", security_answer="20373615")
+
+    def testCase1(self):
+        client = Client()
+        res = client.post(reverse('user:login'), {'id': 114514, 'password': 114514})
+        res_data = res.json()
+        self.assertEqual(res_data.get('errno'), 2003)
+
+    def testCase2(self):
+        client = Client()
+        res = client.post(reverse('user:login'), {'id': 20373615, 'password': 1919810})
+        res_data = res.json()
+        self.assertEqual(res_data.get('errno'), 2002)
+
+
+    def testCase3(self):
+        client = Client()
+        res = client.post(reverse('user:login'), {'id': 20373615, 'password': 20373615})
+        res_data = res.json()
+        self.assertEqual(res_data.get('errno'), 0)
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import *
+app_name = 'user'
 
 urlpatterns = [
     path('register/', register),
-    path('login/', login),
+    path('login/', login, name='login'),
     path('logout/', logout),
     path('info/', info),
     path('issue/', issue),
